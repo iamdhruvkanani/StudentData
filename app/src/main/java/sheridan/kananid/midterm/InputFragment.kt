bffe8eb626ae2b02e9bc512d5cdcf22fc523f6cc
Dhruv Kanani <kananid@sheridancollege.ca>
2020-10-21 09:47:48 -0400

    changed name of fragment layout files

--- app/src/main/java/sheridan/kananid/midterm/InputFragment.kt
+++ app/src/main/java/sheridan/kananid/midterm/InputFragment.kt
@@ -18,7 +18,7 @@ class InputFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_input, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/sheridan/kananid/midterm/OutputFragment.kt
+++ app/src/main/java/sheridan/kananid/midterm/OutputFragment.kt
@@ -18,7 +18,7 @@ class OutputFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_output, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
