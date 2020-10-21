fb2e51357158de335e9bd484cc8d945a37635f55
Dhruv Kanani <kananid@sheridancollege.ca>
2020-10-21 10:00:34 -0400

    added input fragment UI

--- app/src/main/java/sheridan/kananid/midterm/ui/OutputFragment.kt
+++ app/src/main/java/sheridan/kananid/midterm/ui/OutputFragment.kt
@@ -1,4 +1,4 @@
-package sheridan.kananid.midterm
+package sheridan.kananid.midterm.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,25 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import sheridan.kananid.midterm.R
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class InputFragment : Fragment() {
+class OutputFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false)
+        return inflater.inflate(R.layout.fragment_output, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
     }
 }
