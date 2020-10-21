21934df664618f0fe108babbffe85f49c211da9b
Dhruv Kanani <kananid@sheridancollege.ca>
2020-10-21 10:14:57 -0400

    added model class StudentInfo

---
+++ app/src/main/java/sheridan/kananid/midterm/model/StudentInfo.kt
@@ -0,0 +1,5 @@
+package sheridan.kananid.midterm.model
+
+import java.io.Serializable
+
+data class StudentInfo(var studentName: String, var programName: String,var internationalStudent :Boolean): Serializable
